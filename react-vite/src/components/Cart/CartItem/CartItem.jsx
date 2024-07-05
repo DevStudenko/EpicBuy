f8eef72c175cb2b657bbb2f5c3e93a4ca2a170a9
dstudenko <dstudenko@gmail.com>
2024-07-05 19:20:16 -0400

    add update item qty to cart

--- react-vite/src/components/Cart/CartItem/CartItem.jsx
+++ react-vite/src/components/Cart/CartItem/CartItem.jsx
@@ -1,21 +1,28 @@
-import { useDispatch } from "react-redux"
-import { removeCartItemThunk } from "../../../redux/cart";
+import { useDispatch } from "react-redux";
 import styles from "./CartItem.module.css";
 import { IoStar } from "react-icons/io5";
-// import { useSelector } from "react-redux";
-// import { getProductsArray } from "../../../redux/products";
+import { useState } from "react";
+import { updateCartItemThunk, removeCartItemThunk } from "../../../redux/cart";
 
 const CartItem = ({ id, name, preview_img_url, price, quantity, avgRating }) => {
     const dispatch = useDispatch();
-    // const products = useSelector(getProductsArray);
-    // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!products: ', products)
-    // const product = products.filter((product) => product.id === product_id);
-    // console.log('!!!!!!!!!!!!!!!!!!!!product: ', product);
-    // const avgRating = product.avgRating;
+    const [currentQuantity, setCurrentQuantity] = useState(quantity);
+
+    const updateQuantity = (e) => {
+        const newQuantity = parseInt(e.target.value);
+        setCurrentQuantity(newQuantity);
+        if (newQuantity < 1) {
+            dispatch(removeCartItemThunk(id));
+        } else {
+            dispatch(updateCartItemThunk(id, newQuantity));
+        }
+    };
 
     const removeFromCart = () => {
         dispatch(removeCartItemThunk(id));
-    }
+    };
+
+    const qtyOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return (
         <main className={styles.main}>
@@ -24,18 +31,28 @@ const CartItem = ({ id, name, preview_img_url, price, quantity, avgRating }) => 
                     className={styles.item}
                     title={name}
                 >
-                    <img src={preview_img_url} />
+                    <img src={preview_img_url} alt={name} />
                     <div className={styles.product__info}>
                         <div className={styles.item__name}>{name}</div>
                         <div className={styles.item__price}>${price}</div>
-                        <div className={styles.item__rating}><IoStar className={styles.item__starIcon} />{avgRating ? avgRating.toFixed(1) : "New"}</div>
-                        <div className={styles.item__quantity}>quantity: {quantity}</div>
+                        <div className={styles.item__rating}>
+                            <IoStar className={styles.item__starIcon} />
+                            {avgRating ? avgRating.toFixed(1) : "New"}
+                        </div>
+                        <div className={styles.item__quantity}>
+                            Quantity:
+                            <select value={currentQuantity} onChange={updateQuantity}>
+                                {qtyOptions.map((val) => (
+                                    <option value={val} key={val}>{val}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-                    <button onClick={removeFromCart} className={styles.item__remove}>Remove from Basket</button>
+                    <button onClick={removeFromCart} className={styles.item__remove}>Delete</button>
                 </div>
             </div>
         </main>
-    )
-}
+    );
+};
 
-export default CartItem
+export default CartItem;
