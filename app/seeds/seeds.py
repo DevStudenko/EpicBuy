8ce0a0dfe12f91e51053ad7e23609a97916f3cc0
dstudenko <dstudenko@gmail.com>
2024-07-17 19:50:28 -0400

    seed db and run build

--- app/seeds/seeds.py
+++ app/seeds/seeds.py
@@ -6,10 +6,10 @@ from werkzeug.security import generate_password_hash
 def create_seeder():
     ## SEED USERS
     user_list = [
-        {'username':'Demo', 'email':'demo@aa.io', 'password':generate_password_hash("password"), 'profile_img_url':""},
-        {'username':'marnie', 'email':'marnie@aa.io', 'password':generate_password_hash("password"), 'profile_img_url':""},
-        {'username':'john', 'email':'john@aa.io', 'password':generate_password_hash("password"), 'profile_img_url':""},
-        {'username':'admin', 'email':'admin@aa.io', 'password':generate_password_hash("admin"), 'is_admin': True, 'profile_img_url':""}
+        {'username': 'Demo', 'email': 'demo@aa.io', 'password': generate_password_hash("password"), 'profile_img_url': ""},
+        {'username': 'marnie', 'email': 'marnie@aa.io', 'password': generate_password_hash("password"), 'profile_img_url': ""},
+        {'username': 'john', 'email': 'john@aa.io', 'password': generate_password_hash("password"), 'profile_img_url': ""},
+        {'username': 'admin', 'email': 'admin@aa.io', 'password': generate_password_hash("admin"), 'is_admin': True, 'profile_img_url': ""}
     ]
 
     for user_data in user_list:
@@ -25,9 +25,19 @@ def create_seeder():
 
     ## SEED PRODUCTS
     product_list = [
-        {'owner_id': 1, 'name': 'iPhone 13', 'description': 'Latest Apple smartphone', 'price': 99.99, 'preview_img_url': 'https://images.unsplash.com/photo-1638038772924-ef79cce2426d', 'quantity': 20},
-        {'owner_id': 2, 'name': 'MacBook Pro', 'description': 'High-performance laptop from Apple', 'price': 199., 'preview_img_url': 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8', 'quantity': 20},
-        {'owner_id': 3, 'name': 'Apple Watch', 'description': 'Smartwatch with fitness tracking', 'price': 299.00, 'preview_img_url': 'https://images.unsplash.com/photo-1551816230-ef5deaed4a26', 'quantity': 20},
+        {'owner_id': 3, 'name': 'Zenith Blaze', 'description': 'Windows 11 Home AMD Ryzen™ 9 7900X CPU MSI PRO B650-P WIFI 32GB DDR5-6000MHz RAM GeForce RTX 4070 Ti SUPER - 16GB 1TB WD M.2 NVMe SSD', 'price': 2199, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/25755/gaming-pc-01-H7-Flow-Black-main-solo-400.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Inferno Sentinel', 'description': 'Windows 11 Home AMD Ryzen™ 5 7600 CPU ASUS Prime B650M-A AX6 16GB DDR5-5200MHz RAM GeForce RTX 3050 - 6GB 1TB WD M.2 NVMe SSD', 'price': 1399, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/27658/gaming-pc-01-Scale-main-400-solo-(2).png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Quantum Fury', 'description': 'Windows 11 Home Intel® Core™ i7-14700KF CPU ASUS Z790 GAMING WiFi 7 32GB DDR5-6000MHz RAM GeForce RTX 4060 Ti - 8GB 1TB WD M.2 NVMe SSD', 'price': 1799, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/27211/gaming-pc-01-Trace7Mesh-main-400-solo-.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Aurora Titan', 'description': 'Windows 11 Home AMD Ryzen™ 7 7800X3D CPU ASUS Prime B650M-A AX6 32GB DDR5-6000MHz RAM GeForce RTX 4070 SUPER - 12GB 1TB WD M.2 NVMe SSD', 'price': 2099, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/26900/gaming-pc-01-Lancool-216-white-main-400-solo.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Stellar Surge', 'description': 'Windows 11 Home AMD Ryzen™ 9 7900X CPU MSI PRO B650-P WIFI 32GB DDR5-6000MHz RAM AMD Radeon RX 7900 XTX - 24GB 2TB WD M.2 NVMe SSD', 'price': 2499, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/27550/gaming-pc-01-iBP-Y70Black-MainSolo-400.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Hyperion Vanguard', 'description': 'Windows 11 Home AMD Ryzen™ 9 7950X CPU ASUS PRIME X670-P WIFI 32GB DDR5-6000MHz RAM GeForce RTX 4090 - 24GB 2TB WD M.2 NVMe SSD', 'price': 3199, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/27050/gaming-pc-01-H9-Elite-white-main-400-solo.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Quantum Surge', 'description': 'Windows 11 Home Intel Core i5-14400F CPU ASUS Z790 GAMING WiFi 7 32GB DDR5-6000MHz RAM GeForce RTX 3050 - 6GB 1TB WD M.2 NVMe SSD', 'price': 1499, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/17462/SnowblindS-01-400-5.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Cosmic Raptor', 'description': 'Windows 11 Home AMD Ryzen™ 9 7950X CPU MSI PRO B650-P WIFI 32GB DDR5-6000MHz RAM GeForce RTX 4080 SUPER - 16GB 2TB WD M.2 NVMe SSD', 'price': 2799, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/27052/gaming-pc-01-H9-Elite-black-main-400-solo.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Phantom Vortex', 'description': 'Windows 11 Home Intel® Core™ i7-14700KF CPU ASUS Z790 GAMING WiFi 7 32GB DDR5-6000MHz RAM GeForce RTX 4070 Ti SUPER - 16GB 2TB WD M.2 NVMe SSD', 'price': 2399, 'preview_img_url': 'https://content.ibuypower.com/Images/Components/25119/gaming-pc-01-Y60-MainSolo-400.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Nebula Striker', 'description': 'Windows 11 Home Intel Core i5-14400F - 16GB Memory - NVIDIA GeForce RTX 4060 8GB - 1TB SSD - Black', 'price': 1599, 'preview_img_url': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6575/6575072_sd.jpg', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Inferno Pulse', 'description': 'CHASSIS: Black PROCESSOR: AMD Ryzen™ 5 5600G 3.9 - 4.4GHz 6 Cores GRAPHICS CARD TYPE: AMD Radeon™ GRAPHICS CARD: Integrated Radeon RX Vega Graphics* MOTHERBOARD: A520M Motherboard POWER SUPPLY: MSI MPG 750W GOLD DDR4 DIMM MEMORY: 16GB DDR4 3600MHz T-Force Delta RGB (2x 8GB) OPERATING SYSTEM DRIVE: 500GB NVME SSD GEN 3 OPERATING SYSTEM: Windows 11 Home', 'price': 1099, 'preview_img_url': 'https://xoticpc.com/cdn/shop/files/GX13REDEYE05.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Stellar Titan', 'description': 'WINDOWS 11 HOME + USB RECOVERY AMD RYZEN 5 5600 6-CORE NVIDIA GeForce RTX 4060 8GB 16GB DDR4 Dual Channel 1TB NVMe M.2 Cooler Master Hyper 212 Evo CM MasterFans RGB Cooler Master TD500 RGB 750W ATX 80 Plus Gold MSI B550 Series | AMD Wireless 802.11ax', 'price': 1242, 'preview_img_url': 'https://www.buildredux.com/cdn/shop/files/TD500-1-Trans_x480.png', 'quantity': 30},
+        {'owner_id': 3, 'name': 'Galactic Fury', 'description': 'PROCESSOR: Intel® Core™ i9-14900K 3.2 - 6.0GHz 24 Cores GRAPHICS CARD: ASUS ROG Strix GeForce RTX™ 4090 24GB GDDR6X MOTHERBOARD: ASUS ROG MAXIMUS Z790 DARK HERO POWER SUPPLY: ASUS 1200W THOR PLATINUM MEMORY: T-Force Delta RGB Black 32GB DDR5 6000MHz (2X 16GB) OPERATING SYSTEM DRIVE: 2TB NVME SSD GEN 4 OPERATING SYSTEM: Windows 11 Home COOLING: ASUS ROG RYUJIN 360 AIO Liquid Cooler', 'price': 4299, 'preview_img_url': 'https://xoticpc.com/cdn/shop/files/HDMS.png', 'quantity': 30}
     ]
 
     for product_data in product_list:
@@ -48,22 +58,22 @@ def create_seeder():
         {'product_id': 1, 'img_url': 'https://images.unsplash.com/photo-1647503380147-e075b24f4cbe'},
         {'product_id': 2, 'img_url': 'https://images.unsplash.com/photo-1580522154071-c6ca47a859ad'},
         {'product_id': 2, 'img_url': 'https://images.unsplash.com/photo-1542393545-10f5cde2c810'},
-        {'product_id': 3, 'img_url': 'https://images.unsplash.com/photo-1542541864-4abf21a55761'},
+        {'product_id': 3, 'img_url': 'https://images.unsplash.com/photo-1542541864-4abf21a55761'}
     ]
 
     for image_data in image_list:
         image = Image(
             product_id=image_data['product_id'],
-            img_url=image_data['img_url'],
+            img_url=image_data['img_url']
         )
         db.session.add(image)
     db.session.commit()
 
     ## SEED REVIEWS
     review_list = [
-        {'user_id': 1, 'product_id': 1, 'review': 'Great phone!', 'rating': 5},
-        {'user_id': 2, 'product_id': 2, 'review': 'Loving my new laptop', 'rating': 4},
-        {'user_id': 3, 'product_id': 3, 'review': 'Very useful for fitness tracking', 'rating': 5},
+        {'user_id': 1, 'product_id': 1, 'review': 'Great PC!', 'rating': 5},
+        {'user_id': 2, 'product_id': 2, 'review': 'Loving my new setup', 'rating': 4},
+        {'user_id': 3, 'product_id': 3, 'review': 'Very powerful and efficient', 'rating': 5}
     ]
 
     for review_data in review_list:
@@ -71,7 +81,7 @@ def create_seeder():
             user_id=review_data['user_id'],
             product_id=review_data['product_id'],
             review=review_data['review'],
-            rating=review_data['rating'],
+            rating=review_data['rating']
         )
         db.session.add(review)
     db.session.commit()
@@ -80,12 +90,12 @@ def create_seeder():
     cart_list = [
         {'user_id': 1},
         {'user_id': 2},
-        {'user_id': 3},
+        {'user_id': 3}
     ]
 
     for cart_data in cart_list:
         cart = Cart(
-            user_id=cart_data['user_id'],
+            user_id=cart_data['user_id']
         )
         db.session.add(cart)
     db.session.commit()
@@ -93,14 +103,14 @@ def create_seeder():
     cart_item_list = [
         {'cart_id': 1, 'product_id': 1, 'quantity': 1},
         {'cart_id': 2, 'product_id': 2, 'quantity': 1},
-        {'cart_id': 3, 'product_id': 3, 'quantity': 1},
+        {'cart_id': 3, 'product_id': 3, 'quantity': 1}
     ]
 
     for cart_item_data in cart_item_list:
         cart_item = CartItem(
             cart_id=cart_item_data['cart_id'],
             product_id=cart_item_data['product_id'],
-            quantity=cart_item_data['quantity'],
+            quantity=cart_item_data['quantity']
         )
         db.session.add(cart_item)
     db.session.commit()
@@ -109,22 +119,22 @@ def create_seeder():
     favorite_list = [
         {'user_id': 1, 'product_id': 1},
         {'user_id': 2, 'product_id': 2},
-        {'user_id': 3, 'product_id': 3},
+        {'user_id': 3, 'product_id': 3}
     ]
 
     for favorite_data in favorite_list:
         favorite = Favorite(
             user_id=favorite_data['user_id'],
-            product_id=favorite_data['product_id'],
+            product_id=favorite_data['product_id']
         )
         db.session.add(favorite)
     db.session.commit()
 
     ## SEED PURCHASES
     purchase_list = [
-        {'user_id': 1, 'product_id': 1, 'quantity': 1, 'price_at_purchase': 999.00},
-        {'user_id': 2, 'product_id': 2, 'quantity': 1, 'price_at_purchase': 1999.00},
-        {'user_id': 3, 'product_id': 3, 'quantity': 1, 'price_at_purchase': 399.00},
+        {'user_id': 1, 'product_id': 1, 'quantity': 1, 'price_at_purchase': 2199.00},
+        {'user_id': 2, 'product_id': 2, 'quantity': 1, 'price_at_purchase': 1949.00},
+        {'user_id': 3, 'product_id': 3, 'quantity': 1, 'price_at_purchase': 1799.00}
     ]
 
     for purchase_data in purchase_list:
@@ -132,7 +142,7 @@ def create_seeder():
             user_id=purchase_data['user_id'],
             product_id=purchase_data['product_id'],
             quantity=purchase_data['quantity'],
-            price_at_purchase=purchase_data['price_at_purchase'],
+            price_at_purchase=purchase_data['price_at_purchase']
         )
         db.session.add(purchase)
     db.session.commit()
--- migrations/versions/20240717_194846_.py
+++ migrations/versions/20240717_194846_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3dc64b055998
+Revision ID: 2bd329e0755f
 Revises: 
-Create Date: 2024-07-16 23:20:21.711589
+Create Date: 2024-07-17 19:48:46.738840
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3dc64b055998'
+revision = '2bd329e0755f'
 down_revision = None
 branch_labels = None
 depends_on = None
