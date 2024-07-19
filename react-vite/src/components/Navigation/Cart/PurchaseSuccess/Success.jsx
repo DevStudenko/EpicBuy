3fa362ded4d7f935c83f6bfe6f106f064c743fcd
dstudenko <dstudenko@gmail.com>
2024-07-19 19:41:55 -0400

    ran build

--- react-vite/src/components/Navigation/Cart/PurchaseSuccess/Success.jsx
+++ react-vite/src/components/Navigation/Cart/PurchaseSuccess/Success.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import styles from './Success.module.css';
 
